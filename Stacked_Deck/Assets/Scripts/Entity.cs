11a00087c7b70cbf5c74063650709148b8a44e92
ForksTheDawg <mrrollender@gmail.com>
2015-08-29 14:06:51 -0400

    Entity Item Equip added

--- Stacked_Deck/Assets/Scripts/Entity.cs
+++ Stacked_Deck/Assets/Scripts/Entity.cs
@@ -3,13 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 public class Entity : Card
 {
-	public const int MONSTER = 0;
-	public const int CHAMPION = 1;
+	public const int TYPE_MONSTER = 0;
+	public const int TYPE_CHAMPION = 1;
 
 	public int maxHealth;
 	public int health;
 	public int attack;
-	public int entityType;	
+	public int entityType;
+
+	private IList<Item> itemsEquipped;
+	private const int ITEM_MAX = 3;
 
 	public Entity(int ID, int skin, int cost, int entityType, string name, string displayText, int attack, int maxHealth,string effects){
 		this.ID = ID;
@@ -22,6 +25,31 @@ public class Entity : Card
 		this.maxHealth = maxHealth;
 		this.health = this.maxHealth;
 		this.health = this.maxHealth = maxHealth;
+		this.itemsEquipped = new List<Item>();
+	}
+
+	public bool tryEquipItem(Item item)
+	{
+		if(canEquipItem(item))
+		{
+			doEquipItem(item);
+			return true;
+		}
+		return false;
+	}
+
+	private bool canEquipItem(Item item)
+	{
+		if(itemsEquipped.Count < ITEM_MAX)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	private void doEquipItem(Item item)
+	{
+		itemsEquipped.Add(item);
 	}
 	
 	public override void OnBeginDrag (UnityEngine.EventSystems.PointerEventData eventData)
